4158db2afb6184f28a1daf3c9ecc47e2258d1aec
Victoria Chen <missvictoriachen@gmail.com>
2013-08-10 01:30:43 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
